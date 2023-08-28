c4d22f5f93decb4462057ca163c3c548527f12d4
HusseinSaad <hussein.saad2021@gmail.com>
2023-08-28 04:49:18 +0300

    solve the first 7 problems

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr) {
+    for (let i = 0; i < arguments.length; i++) {
+        let index = arr.indexOf(arguments[i]);
+        if (index > -1) {
+            arr.splice(index, 1);
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
 const sumAll = function() {
+  
+    if (typeof arguments[0] !== 'number' || typeof arguments[1] !== 'number')
+        return 'ERROR';
 
+    if (arguments[0] < 0 || arguments[1] < 0)
+        return 'ERROR';
+    
+    let sum = 0;
+    let min = Math.min(arguments[0], arguments[1]);
+    let max = Math.max(arguments[0], arguments[1]);
+
+    sum += max*(max+1)/2;
+    min--;
+    sum += min*(min+1)/2;
+    
+    return sum;  
 };
 
 // Do not edit below this line
